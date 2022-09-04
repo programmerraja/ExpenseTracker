a312dda49938af3273535e80719c281cdf54c4b4
programmerraja <boooathis123@gmail.com>
2022-09-04 18:07:12 +0530

    Added build

---
+++ client/build/precache-manifest.149d0670e549f12d74f3ac69ba813390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9272577eea8425c4e805796e1f55c41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3f5d71644a464eddbe7",
+    "url": "/static/css/main.354f8fe7.chunk.css"
+  },
+  {
+    "revision": "2d3d367e10df4c35142e",
+    "url": "/static/js/2.4548e846.chunk.js"
+  },
+  {
+    "revision": "f5ab9e1a7ad4774baded1e89af7acd58",
+    "url": "/static/js/2.4548e846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f5d71644a464eddbe7",
+    "url": "/static/js/main.4241973c.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
